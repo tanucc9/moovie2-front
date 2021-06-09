58f8e9c3034b175a7bbe78508004a0ca0d76c015
tanucc <maurogaetano997@gmail.com>
2021-06-09 19:20:13 +0200

    Creazione della componente Header

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+          <q-header elevated>
+        <q-toolbar>
+          <q-btn flat round dense icon="menu" class="q-mr-sm" />
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+
+          <q-toolbar-title>Moovie2</q-toolbar-title>
+
+          <transition
+            appear
+            enter-active-class="animated bounceInRight"
+            leave-active-class="animated fadeOut"
+          >
+        <q-input v-if="activeSearchBar" dark v-model="searchText" label="Cerca film..." dense color="white" label-color="white">
+            <template v-slot:append>
+                <q-icon name="search" class="cursor-pointer" color="white"/>
+            </template>
+        </q-input>
+        </transition>
+        
+          <q-btn flat round dense icon="search" v-if="!activeSearchBar" @click="activeSearchBar = !activeSearchBar"/>
+        </q-toolbar>
+
+        <q-tabs v-model="tab">
+          <q-tab name="film" label="Film" />
+          <q-tab name="attori" label="Attori" />
+          <q-tab name="registi" label="Registi" />
+        </q-tabs>
+      </q-header>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 'film',
+      searchText: '',
+      activeSearchBar: false,
+    }
+  }
+}
+</script>
